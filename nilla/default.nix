be7867060db0cff911a8ea6f085c4bab5788bfee
May Munds <realmredwood@gmail.com>
2025-08-18 15:16:17 -0300

    Flake compat for Ignis

--- nilla/default.nix
+++ nilla/default.nix
@@ -1,9 +1,58 @@
 { config, ... }:
 let
-  systems = config.modules.pulsar.defaultSystems;
+  pins = import ../npins;
+
+  nixpkgs-flake = config.inputs.flake-compat.result.load {
+    src = config.inputs.nixpkgs.src;
+  };
+
+  systems = [
+    "x86_64-linux"
+    "aarch64-linux"
+  ];
+
+  loaders = {
+    nixpkgs = "nixpkgs";
+    nilla-cli = "nilla";
+    nilla = "nilla";
+    nix-index-database = "legacy";
+    lix-module = "raw";
+    ignis = "flake";
+    # ignis-gvc = "raw";
+    flake-compat = "legacy";
+  };
+
+  settings = {
+    nixpkgs = {
+      inherit systems;
+      configuration = {
+        allowUnfree = true;
+        # allowBroken = true;
+      };
+    };
+
+    ignis = {
+      inputs = {
+        nixpkgs = nixpkgs-flake;
+      };
+    };
+
+    # nixpkgs-unstable = settings.nixpkgs;
+  };
 in
 {
   config = {
+    inputs = builtins.mapAttrs (name: pin: {
+      src = pin;
+
+      loader = loaders.${name} or { };
+      settings = settings.${name} or { };
+    }) pins;
+
+    modules.pulsar = {
+      defaultSystems = systems;
+    };
+
     shells.default = {
       inherit systems;
 
@@ -35,6 +84,12 @@ in
         inherit systems;
 
         package = import ./packages/apotris;
+      };
+
+      sonicmania = {
+        inherit systems;
+
+        package = import ./packages/sonicmania;
       };
     };
   };
